a2ced72aa963d50a98b10f27cb442188ce7a6cf8
avpuser <avpuser@gmail.com>
2025-04-11 13:40:09 +0300

    telegram bot common utils

---
+++ src/main/java/com/avpuser/telegram_api/TelegramBotRunner.java
@@ -0,0 +1,50 @@
+package com.avpuser.telegram_api;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.generics.BotSession;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+public class TelegramBotRunner {
+
+    private static final Logger logger = LogManager.getLogger(TelegramBotRunner.class);
+
+    private final TelegramLongPollingBot bot;
+
+    private BotSession botSession;
+
+    public TelegramBotRunner(TelegramLongPollingBot bot) {
+        this.bot = bot;
+    }
+
+    public void stop() {
+        if (botSession == null || !botSession.isRunning()) {
+            logger.info(bot.getBotUsername() + " already stopped");
+        }
+        botSession.stop();
+    }
+
+    public void run() {
+        if (botSession != null && botSession.isRunning()) {
+            logger.info(bot.getBotUsername() + " already running");
+            return;
+        }
+
+        TelegramBotsApi botsApi;
+        try {
+            botsApi = new TelegramBotsApi(DefaultBotSession.class);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            botSession = botsApi.registerBot(bot);
+            logger.info(bot.getBotUsername() + " started");
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
---
+++ src/main/java/com/avpuser/telegram_api/TelegramBotUtils.java
@@ -0,0 +1,33 @@
+package com.avpuser.telegram_api;
+
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+import java.util.Optional;
+
+public class TelegramBotUtils {
+
+    public static Optional<Long> getChatId(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            return Optional.of(update.getMessage().getChatId());
+        }
+
+        if (update.getCallbackQuery() != null && update.getCallbackQuery().getMessage() != null) {
+            return Optional.of(update.getCallbackQuery().getMessage().getChatId());
+        }
+
+        return Optional.empty();
+    }
+
+    public static Optional<org.telegram.telegrambots.meta.api.objects.User> getTelegramUser(Update update) {
+        if (update.getMessage() != null) {
+            return Optional.of(update.getMessage().getFrom());
+        }
+
+        if (update.getCallbackQuery() != null && update.getCallbackQuery().getMessage() != null) {
+            return Optional.of(update.getCallbackQuery().getFrom());
+        }
+
+        return Optional.empty();
+    }
+
+}
