a655fdd9cf98892d4e15708ab2c6cc2a9f9ec7dc
avpuser <avpuser@gmail.com>
2025-04-12 17:03:08 +0300

    tg to common utils

--- src/main/java/com/avpuser/telegram/TelegramBotUtils.java
+++ src/main/java/com/avpuser/telegram/TelegramBotUtils.java
@@ -1,6 +1,12 @@
 package com.avpuser.telegram;
 
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.GetFile;
+import org.telegram.telegrambots.meta.api.objects.Document;
+import org.telegram.telegrambots.meta.api.objects.File;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.util.Optional;
 
@@ -27,6 +33,38 @@ public class TelegramBotUtils {
             return Optional.of(update.getCallbackQuery().getFrom());
         }
 
+        return Optional.empty();
+    }
+
+    public static String getFilePath(TelegramLongPollingBot telegramBot, String fileId) {
+        GetFile getFile = new GetFile();
+        getFile.setFileId(fileId);
+        File file;
+        try {
+            file = telegramBot.execute(getFile);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+        return file.getFilePath();
+    }
+
+    public static String getFileUrl(TelegramLongPollingBot telegramBot, String fileId) {
+        return getFileUrl(telegramBot.getBotToken(), getFilePath(telegramBot, fileId));
+    }
+
+    public static String getFileUrl(String botToken, String filePath) {
+        return "https://api.telegram.org/file/bot" + botToken + "/" + filePath;
+    }
+
+
+    public static Optional<Document> getDocumentO(Update update) {
+        if (!update.hasMessage()) {
+            return Optional.empty();
+        }
+        Message message = update.getMessage();
+        if (message.hasDocument()) {
+            return Optional.of(message.getDocument());
+        }
         return Optional.empty();
     }
 
