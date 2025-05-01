04460786b01af3b8a32dee0f67c1fad9bd5c23ab
avpuser <avpuser@gmail.com>
2025-05-01 21:10:51 +0300

    refac ResourceFileUtils

--- src/main/java/com/avpuser/utils/ResourceFileUtils.java
+++ src/main/java/com/avpuser/utils/ResourceFileUtils.java
@@ -7,16 +7,15 @@ import java.nio.charset.StandardCharsets;
 
 public class ResourceFileUtils {
 
-    public static String getTestResourceFilePath(String fileName) {
+    public static String resolveTestResourceFilePath(String fileName) {
         String resourceRoot = "src/test/resources/";
         String projectRoot = new File("").getAbsolutePath();
         String fullPath = projectRoot + File.separator + resourceRoot + fileName;
-
         return fullPath.replace("%20", " ");
     }
 
-    public static String readTestFile(String fileName) {
-        String filePath = getTestResourceFilePath(fileName);
+    public static String readTestResourceFileContent(String fileName) {
+        String filePath = resolveTestResourceFilePath(fileName);
         StringBuilder sb = new StringBuilder();
         try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
             for (String line; (line = br.readLine()) != null; ) {
@@ -31,35 +30,10 @@ public class ResourceFileUtils {
         return sb.toString();
     }
 
-    public static String readFile(String fileName) {
-        String filePath = getTestResourceFilePath(fileName);
-        if (FileUtils.fileExists(filePath)) {
-            return readFromFile(fileName);
-        }
-        return readFileFromResource(fileName);
-    }
-
-    private static String readFromFile(String fileName) {
-        String filePath = getTestResourceFilePath(fileName);
-        StringBuilder sb = new StringBuilder();
-        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
-            for (String line; (line = br.readLine()) != null; ) {
-                if (!line.isEmpty()) {
-                    sb.append(line).append("\n");
-                }
-            }
-            // line is not visible here.
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        return sb.toString();
-    }
-
-    public static String readFileFromResource(String fileName) {
+    public static String readResourceFileContent(String fileName) {
         StringBuilder sb = new StringBuilder();
         ClassPathResource resource = new ClassPathResource(fileName);
         try (InputStream inputStream = resource.getInputStream(); BufferedReader br = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
-
             String line;
             while ((line = br.readLine()) != null) {
                 if (!line.isEmpty()) {
@@ -72,14 +46,4 @@ public class ResourceFileUtils {
 
         return sb.toString();
     }
-
-    public static String getResourceFilePath(String resourcePath) {
-        try {
-            ClassPathResource resource = new ClassPathResource(resourcePath);
-            return resource.getFile().getAbsolutePath();
-        } catch (IOException e) {
-            throw new RuntimeException("Unable to resolve resource path: " + resourcePath, e);
-        }
-    }
-
 }
