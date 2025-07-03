66b3b73aee224947206a599018e54ca78ff9d4a1
avpuser <avpuser@gmail.com>
2025-07-03 16:28:09 +0300

    refac and new spring

--- src/main/java/com/avpuser/utils/FileUtils.java
+++ src/main/java/com/avpuser/utils/FileUtils.java
@@ -220,11 +220,18 @@ public class FileUtils {
         try {
             MessageDigest digest = MessageDigest.getInstance("SHA-256");
             byte[] hashBytes = digest.digest(input.getBytes(StandardCharsets.UTF_8));
-            return DatatypeConverter.printHexBinary(hashBytes).toLowerCase();
+            return bytesToHex(hashBytes);
         } catch (NoSuchAlgorithmException e) {
             throw new RuntimeException(e);
         }
     }
 
+    private static String bytesToHex(byte[] bytes) {
+        StringBuilder hexString = new StringBuilder(2 * bytes.length);
+        for (byte b : bytes) {
+            hexString.append(String.format("%02x", b));
+        }
+        return hexString.toString();
+    }
 
 }
