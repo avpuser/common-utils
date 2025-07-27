b60fc7893e5ba53d6495b5582f8b49e3c1e06aee
avpuser <avpuser@gmail.com>
2025-07-27 15:15:56 +0300

    АÑFileType new ext

--- src/main/java/com/avpuser/file/FileType.java
+++ src/main/java/com/avpuser/file/FileType.java
@@ -1,29 +1,30 @@
 package com.avpuser.file;
 
+import lombok.Getter;
+
 public enum FileType {
-    PDF(".pdf", "application/pdf"),
-    PNG(".png", "image/png"),
-    JPEG(".jpg", "image/jpeg"),
-    GIF(".gif", "image/gif"),
-    BMP(".bmp", "image/bmp"),
-    TIFF(".tif", "image/tiff"),
-    WEBP(".webp", "image/webp"),
-    DOCX(".docx", "application/vnd.openxmlformats-officedocument.wordprocessingml.document"), // добавлено
+    PDF(".pdf", "application/pdf", false),
+    PNG(".png", "image/png", true),
+    JPEG(".jpg", "image/jpeg", true),
+    GIF(".gif", "image/gif", true),
+    BMP(".bmp", "image/bmp", true),
+    TIFF(".tif", "image/tiff", true),
+    WEBP(".webp", "image/webp", true),
+    DOCX(".docx", "application/vnd.openxmlformats-officedocument.wordprocessingml.document", false),
+    TXT(".txt", "text/plain", false),
     ;
 
+    @Getter
     private final String extension;
+    @Getter
     private final String mimeType;
+    @Getter
+    private final boolean isImage;
 
-    FileType(String extension, String mimeType) {
+    FileType(String extension, String mimeType, boolean isImage) {
         this.extension = extension;
         this.mimeType = mimeType;
+        this.isImage = isImage;
     }
 
-    public String getExtension() {
-        return extension;
-    }
-
-    public String getMimeType() {
-        return mimeType;
-    }
 }
